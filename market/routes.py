d643bfa998b48ae657f580662033dadee69c7533
Ananth Rangarajan <rananth@Ananths-MacBook-Pro.local>
2022-12-21 00:03:07 +0530

    updated files

--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, flash
 from market import db
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required, login_manager
 
 
 @app.route("/")
@@ -13,6 +13,7 @@ def home_page():
 
 
 @app.route("/market")
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -29,6 +30,9 @@ def register_page():
         )
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(
+            f'Account created successfully. You are now logged in as: {user_to_create.username}', category='success')
         return redirect(url_for('market_page'))
 
     if form.errors != {}:
